0206a5a32bcb6fea0563a2a9fedc5e89ffda5264
Nishad Raghuvanshi <nishad.raghuvanshi@gmail.com>
2025-08-18 13:59:27 -0700

    Light Mdoe Button Deletetion

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -109,12 +109,6 @@ const Index = () => {
             <Button variant="ghost" size="icon" onClick={() => setShowSearch(!showSearch)} className="text-white hover:bg-white/10">
               <Search className="h-5 w-5" />
             </Button>
-            
-            <Button variant="ghost" size="icon" onClick={() => setTheme(theme === "light" ? "dark" : "light")} className="text-white hover:bg-white/10">
-              <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-              <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-              <span className="sr-only">Toggle theme</span>
-            </Button>
           </div>
         </div>
       </div>
